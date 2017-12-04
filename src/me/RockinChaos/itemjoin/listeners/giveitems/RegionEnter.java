1acfa916be92a7d6cc3fb954e9d1ad709454c430
RockinChaos <JacobGaming@CraftationGaming.com>
2017-12-04 13:12:38 -0800

    Added region-remove itemflag

--- src/me/RockinChaos/itemjoin/listeners/giveitems/RegionEnter.java
+++ src/me/RockinChaos/itemjoin/listeners/giveitems/RegionEnter.java
@@ -40,14 +40,21 @@ public class RegionEnter implements Listener {
 	public void giveOnRegionEnter(PlayerMoveEvent event) {
 		final Player player = event.getPlayer();
 		if (Hooks.hasWorldGuard() == true) {
-				if (!isInRegion(player) && isInRegion.get(player) != null) {
-					isInRegion.remove(player);
-					long delay = ConfigHandler.getConfig("items.yml").getInt("items-Delay") * 10L;
-					Bukkit.getScheduler().scheduleSyncDelayedTask(ItemJoin.getInstance(), new Runnable() {
-						public void run() {
-							removeEnterItems(player);
+			if (!isInRegion(player) && isInRegion.get(player) != null) {
+				isInRegion.remove(player);
+				long delay = ConfigHandler.getConfig("items.yml").getInt("items-Delay") * 10L;
+				Bukkit.getScheduler().scheduleSyncDelayedTask(ItemJoin.getInstance(), new Runnable() {
+					public void run() {
+						removeEnterItems(player, 1);
+						String[] regions = getRegions().toString().split(",");
+						for (String region: regions) {
+							String getRegion = region.replace("[", "").replace("]", "").replace(" ", "");
+							if (!CheckInRegion(player.getLocation(), getRegion)) {
+								setEnterItems(player, region, 2);
+							}
 						}
-					}, delay);
+					}
+				}, delay);
 			}
 		}
 	}
@@ -59,173 +66,197 @@ public class RegionEnter implements Listener {
 		Bukkit.getScheduler().scheduleSyncDelayedTask(ItemJoin.getInstance(), new Runnable() {
 			public void run() {
 				try {
-				setEnterItems(player, region);
-				if (SetItems.getFailCount().get(player) != 0) {
-					boolean Overwrite = ConfigHandler.getConfig("items.yml").getBoolean("items-Overwrite");
-					if (Overwrite == true) {
-						Language.getSendMessage(player, "failedInvFull", SetItems.getFailCount().get(player).toString());
-					} else if (Overwrite == false) {
-						Language.getSendMessage(player, "failedOverwrite", SetItems.getFailCount().get(player).toString());
+					removeEnterItems(player, 2);
+					setEnterItems(player, region, 1);
+					if (SetItems.getFailCount().get(player) != 0) {
+						boolean Overwrite = ConfigHandler.getConfig("items.yml").getBoolean("items-Overwrite");
+						if (Overwrite == true) {
+							Language.getSendMessage(player, "failedInvFull", SetItems.getFailCount().get(player).toString());
+						} else if (Overwrite == false) {
+							Language.getSendMessage(player, "failedOverwrite", SetItems.getFailCount().get(player).toString());
 						}
-					SetItems.removeFailCount(player);
+						SetItems.removeFailCount(player);
+					}
+					PlayerHandler.delayUpdateInventory(player, 15L);
+				} catch (NullPointerException e) {
+					if (ServerHandler.hasDebuggingMode()) { e.printStackTrace(); }
 				}
-				PlayerHandler.delayUpdateInventory(player, 15L);
-				} catch (NullPointerException ex) {}
 				AnimationHandler.refreshItems(player);
 			}
 		}, delay);
 	}
 	
-	public static void removeEnterItems(Player player) {
+	public static void removeEnterItems(Player player, int step) {
 		if (Utils.isConfigurable()) {
 			for (String item: ConfigHandler.getConfigurationSection().getKeys(false)) {
 				ConfigurationSection items = ConfigHandler.getItemSection(item);
-					if(ItemHandler.containsIgnoreCase(items.getString(".triggers"), "region-enter") || ItemHandler.containsIgnoreCase(items.getString(".triggers"), "region enter")
-							|| ItemHandler.containsIgnoreCase(items.getString(".triggers"), "enter-region") || ItemHandler.containsIgnoreCase(items.getString(".triggers"), "enter region")) {
-					if (items.getString(".slot") != null) {
-						String slotlist = items.getString(".slot").replace(" ", "");
-						String[] slots = slotlist.split(",");
-						final String world = player.getWorld().getName();
-						ItemHandler.clearItemID(player);
-						for (String slot: slots) {
-							String ItemID = ItemHandler.getItemID(player, slot);
-							ItemStack inStoredItems = CreateItems.items.get(world + "." + player.getName().toString() + ".items." + ItemID + item);
-							if (Utils.isCustomSlot(slot)) {
-								PlayerInventory inventory = player.getInventory();
-								if (inventory.getHelmet() != null && ItemHandler.isSimilar(inventory.getHelmet(), inStoredItems)) {
-									inventory.setHelmet(null);
-								}
-								if (inventory.getChestplate() != null && ItemHandler.isSimilar(inventory.getChestplate(), inStoredItems)) {
-									inventory.setChestplate(null);
-								}
-								if (inventory.getLeggings() != null && ItemHandler.isSimilar(inventory.getLeggings(), inStoredItems)) {
-									inventory.setLeggings(null);
-								}
-								if (inventory.getBoots() != null && ItemHandler.isSimilar(inventory.getBoots(), inStoredItems)) {
-									inventory.setBoots(null);
-								}
-								if (ServerHandler.hasCombatUpdate() && inventory.getItemInOffHand() != null && ItemHandler.isSimilar(inventory.getItemInOffHand(), inStoredItems)) {
-									inventory.setItemInOffHand(null);
-								}
-							} else if (Utils.isInt(slot)) {
-								PlayerInventory inventory = player.getInventory();
-								HashMap < String, ItemStack[] > inventoryContents = new HashMap < String, ItemStack[] > ();
-								inventoryContents.put(player.getName(), inventory.getContents());
-								for (ItemStack contents: inventoryContents.get(player.getName())) {
-									if (contents != null && ItemHandler.isSimilar(contents, inStoredItems)) {
-										inventory.remove(contents);
-									}
-								}
-								inventoryContents.clear();
-							}
-						}
+				if (step == 1) {
+					if (ItemHandler.containsIgnoreCase(items.getString(".triggers"), "region-enter") || ItemHandler.containsIgnoreCase(items.getString(".triggers"), "region enter") || ItemHandler.containsIgnoreCase(items.getString(".triggers"), "enter-region") || ItemHandler.containsIgnoreCase(items.getString(".triggers"), "enter region")) {
+						toRemove(items, item, player);
 					}
+				} else if (step == 2) {
+					if (ItemHandler.containsIgnoreCase(items.getString(".triggers"), "region-remove") || ItemHandler.containsIgnoreCase(items.getString(".triggers"), "region remove") || ItemHandler.containsIgnoreCase(items.getString(".triggers"), "remove-region") || ItemHandler.containsIgnoreCase(items.getString(".triggers"), "remove region")) {
+						toRemove(items, item, player);
+					}
+				}
 			}
 		}
-	  }
 	}
 	
-	public static void setEnterItems(Player player, String region) {
+	public static void toRemove(ConfigurationSection items, String item, Player player) {
+		if (items.getString(".slot") != null) {
+			String slotlist = items.getString(".slot").replace(" ", "");
+			String[] slots = slotlist.split(",");
+			final String world = player.getWorld().getName();
+			ItemHandler.clearItemID(player);
+			for (String slot: slots) {
+				String ItemID = ItemHandler.getItemID(player, slot);
+				ItemStack inStoredItems = CreateItems.items.get(world + "." + player.getName().toString() + ".items." + ItemID + item);
+				if (Utils.isCustomSlot(slot)) {
+					PlayerInventory inventory = player.getInventory();
+					if (inventory.getHelmet() != null && ItemHandler.isSimilar(inventory.getHelmet(), inStoredItems)) {
+						inventory.setHelmet(null);
+					}
+					if (inventory.getChestplate() != null && ItemHandler.isSimilar(inventory.getChestplate(), inStoredItems)) {
+						inventory.setChestplate(null);
+					}
+					if (inventory.getLeggings() != null && ItemHandler.isSimilar(inventory.getLeggings(), inStoredItems)) {
+						inventory.setLeggings(null);
+					}
+					if (inventory.getBoots() != null && ItemHandler.isSimilar(inventory.getBoots(), inStoredItems)) {
+						inventory.setBoots(null);
+					}
+					if (ServerHandler.hasCombatUpdate() && inventory.getItemInOffHand() != null && ItemHandler.isSimilar(inventory.getItemInOffHand(), inStoredItems)) {
+						inventory.setItemInOffHand(null);
+					}
+				} else if (Utils.isInt(slot)) {
+					PlayerInventory inventory = player.getInventory();
+					HashMap < String, ItemStack[] > inventoryContents = new HashMap < String, ItemStack[] > ();
+					inventoryContents.put(player.getName(), inventory.getContents());
+					for (ItemStack contents: inventoryContents.get(player.getName())) {
+						if (contents != null && ItemHandler.isSimilar(contents, inStoredItems)) {
+							inventory.remove(contents);
+						}
+					}
+					inventoryContents.clear();
+				}
+			}
+		}
+	}
+	
+	public static void setEnterItems(Player player, String region, int step) {
 		if (Utils.isConfigurable()) {
 			for (String item: ConfigHandler.getConfigurationSection().getKeys(false)) {
 				ConfigurationSection items = ConfigHandler.getItemSection(item);
 				final String world = player.getWorld().getName();
-				if (WorldHandler.inWorld(items, world) && ItemHandler.containsIgnoreCase(items.getString(".enabled-regions"), region) && PermissionsHandler.hasPermission(items, item, player)) {
-					if(ItemHandler.containsIgnoreCase(items.getString(".triggers"), "region-enter") || ItemHandler.containsIgnoreCase(items.getString(".triggers"), "region enter")
-							|| ItemHandler.containsIgnoreCase(items.getString(".triggers"), "enter-region") || ItemHandler.containsIgnoreCase(items.getString(".triggers"), "enter region")) {
-					if (items.getString(".slot") != null) {
-						String slotlist = items.getString(".slot").replace(" ", "");
-						String[] slots = slotlist.split(",");
-						ItemHandler.clearItemID(player);
-						for (String slot: slots) {
-							String ItemID = ItemHandler.getItemID(player, slot);
-							if (Utils.isCustomSlot(slot)) {
-								SetItems.setCustomSlots(player, item, slot, ItemID);
-							} else if (Utils.isInt(slot)) {
-								SetItems.setInvSlots(player, item, slot, ItemID);
+				if (WorldHandler.inWorld(items, world) && PermissionsHandler.hasPermission(items, item, player)) {
+					if (step == 1) {
+						if (ItemHandler.containsIgnoreCase(items.getString(".triggers"), "region-enter") || ItemHandler.containsIgnoreCase(items.getString(".triggers"), "region enter") || ItemHandler.containsIgnoreCase(items.getString(".triggers"), "enter-region") || ItemHandler.containsIgnoreCase(items.getString(".triggers"), "enter region")) {
+							if (ItemHandler.containsIgnoreCase(items.getString(".enabled-regions"), region)) {
+								toEnter(items, item, player);
+							}
+						}
+					} else if (step == 2) {
+						if (ItemHandler.containsIgnoreCase(items.getString(".triggers"), "region-remove") || ItemHandler.containsIgnoreCase(items.getString(".triggers"), "region remove") || ItemHandler.containsIgnoreCase(items.getString(".triggers"), "remove-region") || ItemHandler.containsIgnoreCase(items.getString(".triggers"), "remove region")) {
+							if (!ItemHandler.containsIgnoreCase(items.getString(".enabled-regions"), region)) {
+								toEnter(items, item, player);
 							}
 						}
 					}
 				}
 			}
 		}
-	  }
+	}
+	
+	public static void toEnter(ConfigurationSection items, String item, Player player) {
+		if (items.getString(".slot") != null) {
+			String slotlist = items.getString(".slot").replace(" ", "");
+			String[] slots = slotlist.split(",");
+			ItemHandler.clearItemID(player);
+			for (String slot: slots) {
+				String ItemID = ItemHandler.getItemID(player, slot);
+				if (Utils.isCustomSlot(slot)) {
+					SetItems.setCustomSlots(player, item, slot, ItemID);
+				} else if (Utils.isInt(slot)) {
+					SetItems.setInvSlots(player, item, slot, ItemID);
+				}
+			}
+		}
 	}
 	
 	public static boolean isInRegion(Player player) {
 		String[] regions = getRegions().toString().split(",");
-		for (String region : regions) {
+		for (String region: regions) {
 			String getRegion = region.replace("[", "").replace("]", "").replace(" ", "");
 			if (CheckInRegion(player.getLocation(), getRegion) && isInRegion.get(player) == null) {
 				isInRegion.put(player, getRegion);
-		        setItems(player, getRegion);
-		        return true;
+				setItems(player, getRegion);
+				return true;
 			} else if (CheckInRegion(player.getLocation(), getRegion) && !getRegion.equalsIgnoreCase(isInRegion.get(player))) {
 				isInRegion.remove(player);
 				isInRegion.put(player, getRegion);
 				setItems(player, getRegion);
 				return true;
-			} else if (CheckInRegion(player.getLocation(), getRegion) && getRegion.equalsIgnoreCase(isInRegion.get(player))) {	
+			} else if (CheckInRegion(player.getLocation(), getRegion) && getRegion.equalsIgnoreCase(isInRegion.get(player))) {
 				return true;
 			}
 		}
- 		return false;
- 	}
-
+		return false;
+	}
+	
 	public static boolean CheckInRegion(Location playerlocation, String regionname) {
- 		if (regionname == null) {
- 			return true;
- 		}
- 	ApplicableRegionSet set = getWGSet(playerlocation);
+		if (regionname == null) {
+			return true;
+		}
+		ApplicableRegionSet set = getWGSet(playerlocation);
 		if (set == null) {
- 			return false;
- 		}
- 		for (ProtectedRegion r : set) {
- 			if (r.getId().equalsIgnoreCase(regionname)) {
- 				return true;
- 			}
- 		}
- 		return false;
- 	}
- 
- 	private static ApplicableRegionSet getWGSet(Location loc) {
- 		WorldGuardPlugin wg = getWorldGuard();
- 		if (wg == null) {
- 			return null;
- 		}
- 		RegionManager rm = wg.getRegionManager(loc.getWorld());
- 		if (rm == null) {
+			return false;
+		}
+		for (ProtectedRegion r: set) {
+			if (r.getId().equalsIgnoreCase(regionname)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private static ApplicableRegionSet getWGSet(Location loc) {
+		WorldGuardPlugin wg = getWorldGuard();
+		if (wg == null) {
 			return null;
- 		}
- 		return rm.getApplicableRegions(com.sk89q.worldguard.bukkit.BukkitUtil.toVector(loc));
- 	}
- 	
- 	public static HashMap<Player, String> getInRegion() {
- 		return isInRegion;
- 	}
- 	
- 	public static void removeInRegion(Player player) {
- 		isInRegion.remove(player);
- 	}
- 	
- 	public static List<String> getRegions() {
- 		return regions;
- 	}
- 	
- 	public static void saveRegion(String region) {
- 		regions.add(region);
- 	}
- 	
- 	public static void resetRegions() {
- 		regions.clear();
- 	}
- 
- 	public static WorldGuardPlugin getWorldGuard() {
- 		Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin("WorldGuard");
- 
- 		if (plugin == null || !(plugin instanceof WorldGuardPlugin)) {
- 			return null;
- 		}
- 		return (WorldGuardPlugin) plugin;
- 	}
+		}
+		RegionManager rm = wg.getRegionManager(loc.getWorld());
+		if (rm == null) {
+			return null;
+		}
+		return rm.getApplicableRegions(com.sk89q.worldguard.bukkit.BukkitUtil.toVector(loc));
+	}
+	
+	public static HashMap < Player, String > getInRegion() {
+		return isInRegion;
+	}
+	
+	public static void removeInRegion(Player player) {
+		isInRegion.remove(player);
+	}
+	
+	public static List < String > getRegions() {
+		return regions;
+	}
+	
+	public static void saveRegion(String region) {
+		regions.add(region);
+	}
+	
+	public static void resetRegions() {
+		regions.clear();
+	}
+	
+	public static WorldGuardPlugin getWorldGuard() {
+		Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin("WorldGuard");
+		if (plugin == null || !(plugin instanceof WorldGuardPlugin)) {
+			return null;
+		}
+		return (WorldGuardPlugin) plugin;
+	}
 }
