9cbef94d7ecd5cd9d6869578f8025951fa3c11d4
RockinChaos <JacobGaming@CraftationGaming.com>
2018-02-04 22:10:22 -0800

    Fixed permissions with LuckPerms

--- src/me/RockinChaos/itemjoin/handlers/PermissionsHandler.java
+++ src/me/RockinChaos/itemjoin/handlers/PermissionsHandler.java
@@ -5,17 +5,18 @@ import org.bukkit.entity.Player;
 
 public class PermissionsHandler {
 
-	public static String customPermissions(ConfigurationSection items, String item, String world) {
-		if (ConfigHandler.getConfig("config.yml").getBoolean("Items-Permissions") == false || items.getString(".permission-node") == null) {
-			return "itemjoin." + world + "." + item;
+	public static String customPermissions(ConfigurationSection items, String item, String worldName) {
+		if (items.getString(".permission-node") != null) {
+			return items.getString(".permission-node");
 		}
-		return items.getString(".permission-node");
+		return "itemjoin." + worldName + "." + item;
 	}
 
-	public static boolean hasPermission(ConfigurationSection items, String item, Player player) {
+	public static boolean hasItemsPermission(ConfigurationSection items, String item, Player player) {
+		String worldName = player.getWorld().getName();
 		if (ConfigHandler.getConfig("config.yml").getBoolean("Items-Permissions") == false 
-				|| player.hasPermission(PermissionsHandler.customPermissions(items, item, player.getWorld().getName())) 
-				|| player.hasPermission("itemjoin." + player.getWorld() + ".*") || player.hasPermission("itemjoin.*") || player.hasPermission("itemjoin.all")) {
+				|| player.hasPermission(PermissionsHandler.customPermissions(items, item, worldName)) 
+				|| player.hasPermission("itemjoin." + worldName + ".*") || player.hasPermission("itemjoin.all")) {
 			return true;
 		}
 		return false;
