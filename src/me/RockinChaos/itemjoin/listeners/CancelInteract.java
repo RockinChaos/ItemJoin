9bd4862ef58b3a50c6142b01dd54caf7108dabb7
RockinChaos <JacobGaming@CraftationGaming.com>
2018-02-04 22:11:26 -0800

    Fixed cancel-events itemflag.

--- src/me/RockinChaos/itemjoin/listeners/CancelInteract.java
+++ src/me/RockinChaos/itemjoin/listeners/CancelInteract.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -24,7 +25,8 @@ public class CancelInteract implements Listener {
 	private static int cdtime = 0;
 	private static int spamtime = 1;
 	
-	 @EventHandler
+	
+	 @EventHandler(priority=EventPriority.LOWEST)
 	  public void onCancelInteracts(PlayerInteractEvent event) 
 	  {
 	    ItemStack item = event.getItem();
@@ -34,7 +36,7 @@ public class CancelInteract implements Listener {
 	        event.setCancelled(true);
 	        PlayerHandler.updateInventory(player);
 	 }
-  }
+ }
 	 
 	 @EventHandler
 	  public void onUseCooldown(PlayerInteractEvent event) {
