86dd58628fb4c7eaf5b17268fe6109e807de4e32
RockinChaos <JacobGaming@CraftationGaming.com>
2018-07-31 08:41:11 -0700

    Tweaks

--- src/com/sk89q/worldguard/bukkit/WorldGuardPlugin.java
+++
@@ -1,9 +0,0 @@
-package com.sk89q.worldguard.bukkit;
-
-public class WorldGuardPlugin {
-	
-	private final RegionContainer regionContainer = new RegionContainer(this);
-	
-    public RegionContainer getRegionContainer() { return regionContainer; }
-
-}
