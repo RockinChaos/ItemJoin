b1a481eb5fc235a87128cc105e9c345c5c0152ea
RockinChaos <JacobGaming@CraftationGaming.com>
2019-04-19 16:23:29 -0700

    Added variable %player_interact%

--- src/me/RockinChaos/itemjoin/utils/Utils.java
+++ src/me/RockinChaos/itemjoin/utils/Utils.java
@@ -1,13 +1,19 @@
 package me.RockinChaos.itemjoin.utils;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.Statistic;
+import org.bukkit.block.Block;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import de.domedd.betternick.api.nickedplayer.NickedPlayer;
 import me.RockinChaos.itemjoin.handlers.ConfigHandler;
@@ -73,7 +79,29 @@ public class Utils {
 		return ChatColor.translateAlternateColorCodes('&', s).toString();
 	}
 	
-	public static String translateLayout(String name, Player player) {
+    public static String getNearbyPlayer(Player player, int range) {
+        ArrayList<Entity> entities = (ArrayList<Entity>) player.getNearbyEntities(range, range, range);
+        ArrayList<Block> sightBlock = (ArrayList<Block>) player.getLineOfSight((Set<Material>) null, range);
+        ArrayList<Location> sight = new ArrayList<Location>();
+        for (int i = 0;i<sightBlock.size();i++)
+            sight.add(sightBlock.get(i).getLocation());
+        for (int i = 0;i<sight.size();i++) {
+            for (int k = 0;k<entities.size();k++) {
+                if (Math.abs(entities.get(k).getLocation().getX()-sight.get(i).getX())<1.3) {
+                    if (Math.abs(entities.get(k).getLocation().getY()-sight.get(i).getY())<1.5) {
+                        if (Math.abs(entities.get(k).getLocation().getZ()-sight.get(i).getZ())<1.3) {
+                        	if (entities.get(k) instanceof Player) {
+                        	   return entities.get(k).getName();
+                        	}
+                        }
+                    }
+                }
+            }
+        }
+        return "INVALID";
+    }
+	
+	public static String translateLayout(String name, Player player, String...placeHolder) {
 		String playerName = "EXEMPT";
 		
 		if (player != null && MemoryHandler.isBetterNick()) {
@@ -90,6 +118,7 @@ public class Utils {
 		try { name = name.replace("%player_food%", String.valueOf(player.getFoodLevel())); } catch (Exception e) { ServerHandler.sendDebugTrace(e); }
 		try { name = name.replace("%player_health%", String.valueOf(player.getHealth())); } catch (Exception e) { ServerHandler.sendDebugTrace(e); }
 		try { name = name.replace("%player_location%", player.getLocation().getBlockX() + ", " + player.getLocation().getBlockY() + ", " + player.getLocation().getBlockZ() + ""); } catch (Exception e) { ServerHandler.sendDebugTrace(e); } }
+		try { name = name.replace("%player_interact%", getNearbyPlayer(player, 3)); } catch (Exception e) { ServerHandler.sendDebugTrace(e); }
 		if (player == null) { try { name = name.replace("%player%", "CONSOLE"); } catch (Exception e) { ServerHandler.sendDebugTrace(e); } }
 	
 		name = ChatColor.translateAlternateColorCodes('&', name).toString();
