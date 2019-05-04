1a703c4946917ce6c97cc11c4ed6e76e218d32dc
RockinChaos <JacobGaming@CraftationGaming.com>
2019-05-04 16:17:54 -0700

    Bug fixes

--- src/me/RockinChaos/itemjoin/utils/Utils.java
+++ src/me/RockinChaos/itemjoin/utils/Utils.java
@@ -80,25 +80,27 @@ public class Utils {
 	}
 	
     public static String getNearbyPlayer(Player player, int range) {
-        ArrayList<Entity> entities = (ArrayList<Entity>) player.getNearbyEntities(range, range, range);
-        ArrayList<Block> sightBlock = (ArrayList<Block>) player.getLineOfSight((Set<Material>) null, range);
-        ArrayList<Location> sight = new ArrayList<Location>();
-        for (int i = 0;i<sightBlock.size();i++)
-            sight.add(sightBlock.get(i).getLocation());
-        for (int i = 0;i<sight.size();i++) {
-            for (int k = 0;k<entities.size();k++) {
-                if (Math.abs(entities.get(k).getLocation().getX()-sight.get(i).getX())<1.3) {
-                    if (Math.abs(entities.get(k).getLocation().getY()-sight.get(i).getY())<1.5) {
-                        if (Math.abs(entities.get(k).getLocation().getZ()-sight.get(i).getZ())<1.3) {
-                        	if (entities.get(k) instanceof Player) {
-                        	   return entities.get(k).getName();
-                        	}
-                        }
-                    }
-                }
-            }
-        }
-        return "INVALID";
+    	try {
+	        ArrayList<Entity> entities = (ArrayList<Entity>) player.getNearbyEntities(range, range, range);
+	        ArrayList<Block> sightBlock = (ArrayList<Block>) player.getLineOfSight((Set<Material>) null, range);
+	        ArrayList<Location> sight = new ArrayList<Location>();
+	        for (int i = 0;i<sightBlock.size();i++)
+	            sight.add(sightBlock.get(i).getLocation());
+	        for (int i = 0;i<sight.size();i++) {
+	            for (int k = 0;k<entities.size();k++) {
+	                if (Math.abs(entities.get(k).getLocation().getX()-sight.get(i).getX())<1.3) {
+	                    if (Math.abs(entities.get(k).getLocation().getY()-sight.get(i).getY())<1.5) {
+	                        if (Math.abs(entities.get(k).getLocation().getZ()-sight.get(i).getZ())<1.3) {
+	                        	if (entities.get(k) instanceof Player) {
+	                        	   return entities.get(k).getName();
+	                        	}
+	                        }
+	                    }
+	                }
+	            }
+	        }
+	        return "INVALID";
+    	} catch (NullPointerException e) { return "INVALID"; }
     }
 	
 	public static String translateLayout(String name, Player player, String...placeHolder) {
