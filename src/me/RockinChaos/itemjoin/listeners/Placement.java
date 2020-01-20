d0c8440a1935a129a1fa869e910244f9e5c0440a
RockinChaos <JacobGaming@CraftationGaming.com>
2020-01-20 01:14:28 -0800

    count-lock itemflag fixes

--- src/me/RockinChaos/itemjoin/listeners/Placement.java
+++ src/me/RockinChaos/itemjoin/listeners/Placement.java
@@ -1,5 +1,6 @@
 package me.RockinChaos.itemjoin.listeners;
 
+import me.RockinChaos.itemjoin.ItemJoin;
 import me.RockinChaos.itemjoin.giveitems.utils.ItemMap;
 import me.RockinChaos.itemjoin.giveitems.utils.ItemUtilities;
 import me.RockinChaos.itemjoin.handlers.PlayerHandler;
@@ -12,8 +13,9 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
 
-public class Placement implements Listener{
+public class Placement implements Listener {
 
 	 @EventHandler
 	 private void onPreventPlayerPlace(PlayerInteractEvent event) {
@@ -29,16 +31,22 @@ public class Placement implements Listener{
 	 private void onCountLock(PlayerInteractEvent event) {
 	 	ItemStack item = event.getItem();
 	 	Player player = event.getPlayer();
-	 	if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK && PlayerHandler.isCreativeMode(player)) {
+	 	if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK && !PlayerHandler.isCreativeMode(player)) {
 	 		if (!ItemUtilities.isAllowed(player, item, "count-lock")) {
 	 			ItemMap itemMap = ItemUtilities.getItemMap(item, null, player.getWorld());
-	 			if (itemMap != null) { item.setAmount(itemMap.getCount()); }
+	 			item.setAmount(itemMap.getCount());
+	 			new BukkitRunnable() {
+	 				@Override
+	 				public void run() {
+	 					if (itemMap != null) { if (itemMap.isSimilar(PlayerHandler.getHandItem(player))) { PlayerHandler.getHandItem(player).setAmount(itemMap.getCount()); } }
+	 				}
+	 			}.runTaskLater(ItemJoin.getInstance(), 2L);
 	 		}
 	 	}
 	 }
 	 
 	 @EventHandler
-	 private void onItemFramePlace(PlayerInteractEntityEvent event) {
+	 private void onFramePlace(PlayerInteractEntityEvent event) {
 	 	if (event.getRightClicked() instanceof ItemFrame) {
 	 		try {
 	 			ItemStack item = null;
@@ -54,7 +62,7 @@ public class Placement implements Listener{
 	 }
 	 
 	 @EventHandler
-	 private void onItemFrameCountLock(PlayerInteractEntityEvent event) {
+	 private void onFrameLock(PlayerInteractEntityEvent event) {
 	 	if (event.getRightClicked() instanceof ItemFrame) {
 	 		try {
 	 			ItemStack item = null;
