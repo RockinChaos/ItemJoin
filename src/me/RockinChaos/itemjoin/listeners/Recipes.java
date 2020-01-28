a3afcb36d6151edb62a822572acb0daabf711562
RockinChaos <JacobGaming@CraftationGaming.com>
2020-01-27 23:23:50 -0800

    item-craftable fixes

--- src/me/RockinChaos/itemjoin/listeners/Recipes.java
+++ src/me/RockinChaos/itemjoin/listeners/Recipes.java
@@ -1,7 +1,6 @@
 package me.RockinChaos.itemjoin.listeners;
 
 import org.bukkit.Material;
-import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -17,22 +16,18 @@ public class Recipes implements Listener {
 	
     @EventHandler
     private void onPlayerCraft(PrepareItemCraftEvent event) {
-        for (HumanEntity entity: event.getViewers()) {
-            if (entity instanceof Player) {
-                Player player = (Player) entity;
-                for (int i = 0; i < player.getOpenInventory().getTopInventory().getSize(); i++) {
-                    if (player.getOpenInventory().getTopInventory().getItem(i) != null && player.getOpenInventory().getTopInventory().getItem(i).getType() != Material.AIR) {
-                        if (!ItemUtilities.isAllowed(player, player.getOpenInventory().getTopInventory().getItem(i), "item-craftable")) {
-                            ItemStack reAdd = player.getOpenInventory().getTopInventory().getItem(i).clone();
-                            player.getOpenInventory().getTopInventory().setItem(i, null);
-                            player.getInventory().addItem(reAdd);
-                            PlayerHandler.updateInventory(player);
-                            break;
-                        }
-                    }
-                }
-            }
-        }
+    	Player player = (Player) event.getInventory().getHolder();
+    	for (int i = 0; i < player.getOpenInventory().getTopInventory().getSize(); i++) {
+    		if (player.getOpenInventory().getTopInventory().getItem(i) != null && player.getOpenInventory().getTopInventory().getItem(i).getType() != Material.AIR) {
+    			if (!ItemUtilities.isAllowed(player, player.getOpenInventory().getTopInventory().getItem(i), "item-craftable")) {
+    				ItemStack reAdd = player.getOpenInventory().getTopInventory().getItem(i).clone();
+    				player.getOpenInventory().getTopInventory().setItem(i, null);
+    				player.getInventory().addItem(reAdd);
+    				PlayerHandler.updateInventory(player);
+    				break;
+    			}
+    		}
+    	}
     }
   
 	@EventHandler
