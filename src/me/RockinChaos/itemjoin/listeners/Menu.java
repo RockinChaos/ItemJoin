224daf92a8440f75283f568fdca34fa418da31f4
RockinChaos <JacobGaming@CraftationGaming.com>
2020-03-14 22:32:03 -0700

    Fixes

--- src/me/RockinChaos/itemjoin/listeners/Menu.java
+++ src/me/RockinChaos/itemjoin/listeners/Menu.java
@@ -56,7 +56,6 @@ public class Menu implements Listener {
 			}.runTaskLater(ItemJoin.getInstance(), 40L);
 		}
 	}
-
 //  ===============================================================================
 
 }
