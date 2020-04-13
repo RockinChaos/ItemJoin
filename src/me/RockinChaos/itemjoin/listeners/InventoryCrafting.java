25e7281e394f4af6d851a41139b68db0b2b9e7ba
RockinChaos <JacobGaming@CraftationGaming.com>
2020-04-13 16:16:16 -0700

    Self-drops bug fixes

--- src/me/RockinChaos/itemjoin/listeners/InventoryCrafting.java
+++ src/me/RockinChaos/itemjoin/listeners/InventoryCrafting.java
@@ -1,3 +1,20 @@
+/*
+ * ItemJoin
+ * Copyright (C) CraftationGaming <https://www.craftationgaming.com/>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package me.RockinChaos.itemjoin.listeners;
 
 import java.util.Collection;
@@ -138,20 +155,15 @@ public class InventoryCrafting implements Listener {
     private void onCraftingDrop(PlayerDropItemEvent event) {
     	final Player player = (Player) event.getPlayer();
     	final ItemStack item = event.getItemDrop().getItemStack().clone();
-    	if (!player.isDead()) {
+    	if (!player.isDead() && !event.isCancelled()) {
 	    	event.getItemDrop().getItemStack().setItemMeta(null);
 	    	event.getItemDrop().remove();
 	    	Bukkit.getScheduler().scheduleSyncDelayedTask(ItemJoin.getInstance(), new Runnable() {
 	    		@Override
 	    		public void run() {
-	    			boolean isCrafting = false;
-	    			if (worldSwitch.containsKey(PlayerHandler.getPlayerID(player)) && worldSwitch.get(PlayerHandler.getPlayerID(player))) {
-	    				for (ItemMap itemMap: ItemUtilities.getItems()) {
-	    					if (itemMap.isCraftingItem() && itemMap.isSimilar(item)) {
-	    						isCrafting = true;
-	    						break;
-	    					}
-	    				}
+	    			ItemMap itemMap = ItemUtilities.getItemMap(item, null, player.getWorld());
+	    			if (itemMap != null && !event.isCancelled() && worldSwitch.containsKey(PlayerHandler.getPlayerID(player)) && worldSwitch.get(PlayerHandler.getPlayerID(player))) {
+		    			boolean isCrafting = itemMap.isCraftingItem();
 	    				if (!isCrafting) {
 	    					dropItem(player, item);
 	    				}
