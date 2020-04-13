01000f58134fedf2a2a4531adf9cec43f45dc495
RockinChaos <JacobGaming@CraftationGaming.com>
2020-04-13 16:16:33 -0700

    Limit-modes bug fixes

--- src/me/RockinChaos/itemjoin/giveitems/listeners/LimitSwitch.java
+++ src/me/RockinChaos/itemjoin/giveitems/listeners/LimitSwitch.java
@@ -1,3 +1,20 @@
+/*
+ * ItemJoin
+ * Copyright (C) CraftationGaming <https://www.craftationgaming.com/>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package me.RockinChaos.itemjoin.giveitems.listeners;
 
 import org.bukkit.GameMode;
@@ -5,58 +22,27 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerGameModeChangeEvent;
-import org.bukkit.scheduler.BukkitRunnable;
-
-import me.RockinChaos.itemjoin.ItemJoin;
-import me.RockinChaos.itemjoin.giveitems.utils.ItemMap;
 import me.RockinChaos.itemjoin.giveitems.utils.ItemUtilities;
 import me.RockinChaos.itemjoin.handlers.ConfigHandler;
-import me.RockinChaos.itemjoin.handlers.PlayerHandler;
-import me.RockinChaos.itemjoin.utils.Chances;
-import me.RockinChaos.itemjoin.utils.Utils;
 
 public class LimitSwitch implements Listener {
 
+   /**
+	* Called on player changing gamemodes.
+	* Gives any available custom items upon changing gamemodes.
+	* Removes any limited custom items upon changing gamemodes.
+	* 
+	* @param event - PlayerGameModeChangeEvent
+	*/
 	@EventHandler
-	private void giveOnGameModeSwitch(PlayerGameModeChangeEvent event) {
+	private void setGameModeItems(PlayerGameModeChangeEvent event) {
 		final Player player = event.getPlayer();
 		final GameMode newMode = event.getNewGameMode();
-		if (ConfigHandler.getDepends().authMeEnabled()) { this.setAuthenticating(player, newMode); } 
-		else { this.setItems(player, newMode); }
-	}
-	
-	private void setAuthenticating(final Player player, final GameMode newMode) {
-		new BukkitRunnable() {
-			@Override
-			public void run() {
-				if (ConfigHandler.getDepends().authMeEnabled() && fr.xephi.authme.api.v3.AuthMeApi.getInstance().isAuthenticated(player)) {
-			    	setItems(player, newMode);
-					this.cancel();
-				}
-			}
-		}.runTaskTimer(ItemJoin.getInstance(), 0, 20);
-	}
-	
-	private void setItems(final Player player, final GameMode newMode) {
-		ItemUtilities.safeSet(player, "Limit-Modes");
-		this.runTask(player, newMode);
-	}
-	
-	private void runTask(final Player player, final GameMode newMode) {
-		final Chances probability = new Chances();
-		final ItemMap probable = probability.getRandom(player);
-		final int session = Utils.getRandom(1, 100000);
-			for (ItemMap item : ItemUtilities.getItems()) { 
-				if (item.isUseOnLimitSwitch() && item.inWorld(player.getWorld()) 
-						&& probability.isProbability(item, probable) && ConfigHandler.getSQLData().isEnabled(player)
-						&& item.hasPermission(player) && ItemUtilities.isObtainable(player, item, session)) {
-					item.giveTo(player, false, 0); 
-					item.setAnimations(player);
-				} else if (item.isUseOnLimitSwitch() && !item.isLimitMode(newMode) && item.inWorld(player.getWorld()) && item.hasItem(player)) {
-					item.removeFrom(player, 0);
-				}
-			}
-		ItemUtilities.sendFailCount(player, session);
-		PlayerHandler.delayUpdateInventory(player, 15L);
+		final ItemUtilities.TriggerType type = ItemUtilities.TriggerType.LIMITSWITCH;
+		if (ConfigHandler.getDepends().authMeEnabled()) { 
+			ItemUtilities.setAuthenticating(player, type, newMode, "GLOBAL"); 
+	    } else { 
+			ItemUtilities.setItems(player, type, newMode, "GLOBAL"); 
+		}
 	}
 }
