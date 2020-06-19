6a42ee7c15858de704ae136b55b700a9442f22b7
RockinChaos <JacobGaming@CraftationGaming.com>
2020-06-18 22:57:11 -0700

    Grindstone support for item-store

--- src/me/RockinChaos/itemjoin/listeners/Storable.java
+++ src/me/RockinChaos/itemjoin/listeners/Storable.java
@@ -52,7 +52,7 @@ public class Storable implements Listener {
 		else { item = event.getCursor(); }
 		if (invType != null) {
 			if (event.getRawSlot() > event.getInventory().getSize() && event.getAction().equals(InventoryAction.MOVE_TO_OTHER_INVENTORY) || event.getRawSlot() < event.getInventory().getSize()) {
-				if ((invType.contains("CHEST") || invType.contains("FURNACE") || invType.contains("SHULKER_BOX") || invType.contains("HOPPER") || invType.contains("ANVIL") || invType.contains("WORKBENCH") || invType.contains("DISPENSER") || invType.contains("DROPPER")) && !ItemUtilities.getUtilities().isAllowed(player, item, "item-store")) {
+				if ((invType.contains("CHEST") || invType.contains("FURNACE") || invType.contains("GRINDSTONE") || invType.contains("SHULKER_BOX") || invType.contains("HOPPER") || invType.contains("ANVIL") || invType.contains("WORKBENCH") || invType.contains("DISPENSER") || invType.contains("DROPPER")) && !ItemUtilities.getUtilities().isAllowed(player, item, "item-store")) {
 					event.setCancelled(true);
 					PlayerHandler.getPlayer().updateInventory(player, 1L);
 				} else if ((invType.contains("ENCHANTING") || invType.contains("ANVIL")) && !ItemUtilities.getUtilities().isAllowed(player, item, "item-modifiable")) {
@@ -77,7 +77,7 @@ public class Storable implements Listener {
 		for (int i: event.getRawSlots()) {
 			if (i < inventorySize) {
 				if (invType != null) {
-					if ((invType.contains("CHEST") || invType.contains("FURNACE") || invType.contains("SHULKER_BOX") 
+					if ((invType.contains("CHEST") || invType.contains("FURNACE") || invType.contains("GRINDSTONE") || invType.contains("SHULKER_BOX") 
 						|| invType.contains("HOPPER") || invType.contains("ANVIL") || invType.contains("WORKBENCH") || invType.contains("DISPENSER") || invType.contains("DROPPER")) && !ItemUtilities.getUtilities().isAllowed(player, item, "item-store")) {
 						event.setCancelled(true);
 						PlayerHandler.getPlayer().updateInventory(player, 1L);
